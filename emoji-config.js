85ac8a81d3368053bdfad3004e870092675f69b1
Nguyen Ngoc Quynh Anh <nthong1895@gmail.com>
2025-10-20 14:39:52 +0700

    Add emoji configuration for reactions

---
+++ emoji-config.js
@@ -0,0 +1,24 @@
+// Cấu hình emoji reactions
+// Thay đổi đường dẫn ảnh theo ảnh bạn upload
+export const REACTION_EMOJIS = [
+  {
+    id: "emoji1",
+    name: "Thích",
+    image: "./emoji1.png", 
+  },
+  {
+    id: "emoji2",
+    name: "Yêu",
+    image: "./emoji2.png", 
+  },
+  {
+    id: "emoji3",
+    name: "Vui",
+    image: "./emoji3.png", 
+  },
+  {
+    id: "emoji4",
+    name: "Tuyệt vời",
+    image: "./emoji4.png", 
+  },
+];
